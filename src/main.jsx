0147b3c6cedb2f959b2245f0ed4a711a94d8b02e
azazelxo0 <amitsuraj2001@gmail.com>
2024-09-26 21:41:00 +0530

    mod

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './bootstrap.min.css'
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
 
